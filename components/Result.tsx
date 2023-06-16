5d3daf409299b5bcefc04a5b15d366171e96d989
fahmid <fahmidhasankonok@gmail.com>
2023-06-16 14:19:28 +0600

    add media query

--- components/Result.tsx
+++ components/Result.tsx
@@ -5,8 +5,8 @@ const Result = ( { MainValue }:any ) => {
     <>
      <div className="container">
       <div className="result_content flex flex-col gap-5">
-        <h2 className='text-4xl font-extrabold text-green-500'>আপনার যাকাতের পরিমাণ :  </h2>
-        <h1>{MainValue}</h1>
+        <h2 className='text-2xl font-extrabold text-green-500'>আপনার যাকাতের পরিমাণ :  </h2>
+        <h1>{MainValue} টাকা</h1>
       </div>
      </div>
     </>
